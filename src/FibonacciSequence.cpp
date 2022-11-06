032d6d927e106d348dcf7b76f80b316e9f3a7034
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-11-05 22:41:40 -0300

    Update FibonacciSequence.cpp

--- src/FibonacciSequence.cpp
+++ src/FibonacciSequence.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	long double numIn, TermA{ 0 }, TermB{ 1 }, FibSeq;
+	int numIn;
+	long double TermA{ 0 }, TermB{ 1 }, FibSeq;
 
 	system("CLS");
 
@@ -33,20 +34,3 @@ int main()
 	system("PAUSE");
 	return 0;
 }
-
-
-/*int FibSeq[1000] {};
-FibSeq[0] = 0;
-FibSeq[1] = 1;
-
-for (i = 2; i < numIn; ++i)
-{
-	FibSeq[i] = FibSeq[i - 1] + FibSeq[i - 2];
-
-}
-std::cout << "\nFibonacci Sequence: " << FibSeq[i];
-for (i = 0; i < numIn; ++i)
-{
-	std::cout << FibSeq[i] << ", ";
-
-}*/
